16dd52285f0d0b5ebf19c4108065a0fdc6e5b30f
dialite <raymondolisa01@gmail.com>
2025-03-05 13:48:57 +0100

    Create Hero section UI

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import HeroSection from "./HeroSection";
 
 const Landing = () => {
   return (
     <div>
-      Landing
+      <HeroSection />
     </div>
   );
-}
+};
 
 export default Landing;
